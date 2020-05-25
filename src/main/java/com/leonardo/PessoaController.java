9c63862b3a21bce896bbe3e2fc3e7809eaf6f448
leonardofaria00 <leonardofaria00@gmail.com>
2020-05-25 18:37:02 -0300

    [UPDATE] - Listando Pessoa por id

--- src/main/java/com/leonardo/PessoaController.java
+++ src/main/java/com/leonardo/PessoaController.java
@@ -1,10 +1,13 @@
 package com.leonardo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,15 @@ public class PessoaController {
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public Pessoa cadastrar(@RequestBody Pessoa pessoa) {
 		return service.cadastrar(pessoa);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Pessoa> buscar(@PathVariable Long id) {
+		Optional<Pessoa> pessoa = service.findById(id);
+
+		if (pessoa.isPresent())
+			return ResponseEntity.ok(pessoa.get());
+
+		return ResponseEntity.notFound().build();
 	}
 }
