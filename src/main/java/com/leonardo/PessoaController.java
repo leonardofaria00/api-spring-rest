032ab0c2b7c23a96a692ba9e5b821c9c795b149e
leonardofaria00 <leonardofaria00@gmail.com>
2020-05-25 18:17:28 -0300

    [UPDATE] - Adicionando Camada de Controler com métodos listar e cadastrar

---
+++ src/main/java/com/leonardo/PessoaController.java
@@ -0,0 +1,31 @@
+package com.leonardo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+	@Autowired
+	private PessoaService service;
+
+	@GetMapping
+	public List<Pessoa> listar() {
+		return service.listar();
+	}
+
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Pessoa cadastrar(@RequestBody Pessoa pessoa) {
+		return service.cadastrar(pessoa);
+	}
+}
