23a54632fad00005aa64f116da3795ffedb17fab
leonardofaria00 <leonardofaria00@gmail.com>
2020-06-22 11:38:07 -0300

    [Feature] Adicionando Handling Exception

---
+++ src/main/java/com/dominio/api/exception/NegocioException.java
@@ -0,0 +1,11 @@
+package com.dominio.api.exception;
+
+public class NegocioException extends Exception {
+
+	public NegocioException(String mensagem) {
+		super(mensagem);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
