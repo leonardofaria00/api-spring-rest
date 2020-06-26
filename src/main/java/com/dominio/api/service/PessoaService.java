1f1694f1dd2c1f710b93f68c21cd37ce5501b36b
leonardofaria00 <leonardofaria00@gmail.com>
2020-06-26 18:23:34 -0300

    [FEATURE] Tratando erros de retorno

--- src/main/java/com/dominio/api/service/PessoaService.java
+++ src/main/java/com/dominio/api/service/PessoaService.java
@@ -5,18 +5,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.dominio.api.dto.PessoaDTO;
+import com.dominio.api.model.Pessoa;
+import com.dominio.api.repository.PessoaRepository;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.dominio.api.dto.PessoaDTO;
-import com.dominio.api.model.Pessoa;
-import com.dominio.api.repository.PessoaRepository;
-
 @Service
-public class PessoaService implements CrudAPI<Pessoa, PessoaDTO> {
+public class PessoaService implements CrudAPIService<Pessoa, PessoaDTO> {
 
 	@Autowired
 	private PessoaRepository repository;
@@ -29,12 +29,15 @@ public class PessoaService implements CrudAPI<Pessoa, PessoaDTO> {
 
 		List<Pessoa> pessoas = repository.findAll();
 
-		List<PessoaDTO> dto = toListDTO(pessoas);
-		return ResponseEntity.ok().body(dto);
+		if (!pessoas.isEmpty()) {
+			List<PessoaDTO> dto = toListDTO(pessoas);
+			return ResponseEntity.ok().body(dto);
+		}
+		return ResponseEntity.noContent().build();
 	}
 
 	@Override
-	public ResponseEntity<Pessoa> cadastrar(Pessoa pessoa) {
+	public ResponseEntity<PessoaDTO> cadastrar(Pessoa pessoa) {
 
 		pessoa.setCadastro(OffsetDateTime.now());
 
@@ -48,36 +51,41 @@ public class PessoaService implements CrudAPI<Pessoa, PessoaDTO> {
 
 		Optional<Pessoa> pessoa = repository.findById(id);
 
-		PessoaDTO dto = toDTO(pessoa.get());
-
-		if (pessoa.isPresent())
+		if (pessoa.isPresent()) {
+			PessoaDTO dto = toDTO(pessoa.get());
 			return ResponseEntity.ok().body(dto);
+		}
+		return ResponseEntity.noContent().build();
+	}
 
-		return ResponseEntity.notFound().build();
+	@Override
+	public ResponseEntity<PessoaDTO> atualizar(Long id, Pessoa pessoa) {
+		if (repository.existsById(id)) {
+			pessoa.setId(id);
+			repository.save(pessoa);
+			return ResponseEntity.status(HttpStatus.OK).build();// Demostrando outras formas de implementar
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();// Demostrando outras formas de implementar
 	}
 
 	@Override
 	public ResponseEntity<Void> remover(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
 
-		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		return ResponseEntity.noContent().build();
 	}
 
 	@Override
-	public ResponseEntity<Pessoa> atualizar(Long id, Pessoa pessoa) {
-		if (repository.existsById(id)) {
-			pessoa.setId(id);
-			repository.save(pessoa);
-			return ResponseEntity.status(HttpStatus.OK).build();
-		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-	}
-
-	private PessoaDTO toDTO(Pessoa pessoa) {
+	public PessoaDTO toDTO(Pessoa pessoa) {
 		return modelmapper.map(pessoa, PessoaDTO.class);
 	}
 
-	private List<PessoaDTO> toListDTO(List<Pessoa> pessoas) {
+	@Override
+	public List<PessoaDTO> toListDTO(List<Pessoa> pessoas) {
 		return pessoas.stream().map(pessoa -> toDTO(pessoa)).collect(Collectors.toList());
 	}
 }
