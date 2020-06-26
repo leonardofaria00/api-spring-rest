a34610565b6d74b406895af8906c0b0044abcec7
leonardofaria00 <leonardofaria00@gmail.com>
2020-06-26 18:14:42 -0300

    [FEATURE] Incluindo convert DTO

--- src/main/java/com/dominio/api/service/CrudAPIService.java
+++ src/main/java/com/dominio/api/service/CrudAPIService.java
@@ -4,16 +4,20 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
-public interface CrudAPI<E, DTO> {
+public interface CrudAPIService<E, DTO> {
 
 	ResponseEntity<List<DTO>> listar();
 
-	ResponseEntity<E> cadastrar(E o);
+	ResponseEntity<DTO> cadastrar(E entity);
 
 	ResponseEntity<DTO> buscar(Long id);
 
 	ResponseEntity<Void> remover(Long id);
 
-	ResponseEntity<E> atualizar(Long id, E pessoa);
+	ResponseEntity<DTO> atualizar(Long id, E entity);
+
+	DTO toDTO(E entity);
+
+	List<DTO> toListDTO(List<E> entity);
 
 }
