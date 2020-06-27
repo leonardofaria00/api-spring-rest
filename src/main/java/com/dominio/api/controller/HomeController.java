768a2a0e20bc4e4a56d396f7cf80cd544414eef7
leonardofaria00 <leonardofaria00@gmail.com>
2020-06-26 21:53:56 -0300

    [UPDATE] Testando path /

--- src/main/java/com/dominio/api/controller/HomeController.java
+++ src/main/java/com/dominio/api/controller/HomeController.java
@@ -1,15 +1,19 @@
 package com.dominio.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/")
 public class HomeController {
 
+	private static final String template = "Hello, %s!";
+
 	@GetMapping
-	public String index() {
-		return "ok";
+	public ResponseEntity<String> index(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return ResponseEntity.ok(String.format(template, name));
 	}
 }
