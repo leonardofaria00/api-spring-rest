c278439073dee054bc5836bb38d98e948d01ffac
leonardofaria00 <leonardofaria00@gmail.com>
2020-06-26 21:51:55 -0300

    [UPDATE] Adicionando Class de Config CORS

---
+++ src/main/java/com/dominio/api/core/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.dominio.api.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurer crosConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry cors) {
+				cors.addMapping("pathPattern").allowedOrigins("http://localhost:9000");
+			}
+		};
+	}
+}
