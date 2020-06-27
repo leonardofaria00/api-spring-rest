66ff06a6226fce7fd98689b70f2980c01dd0d381
leonardofaria00 <leonardofaria00@gmail.com>
2020-06-26 21:52:42 -0300

    [UPDATE] Implementando Lombok

--- src/main/java/com/dominio/api/dto/PessoaDTO.java
+++ src/main/java/com/dominio/api/dto/PessoaDTO.java
@@ -1,24 +1,12 @@
 package com.dominio.api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PessoaDTO {
 
 	private Long id;
-
 	private String nome;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 }
--- src/main/java/com/dominio/api/model/Pessoa.java
+++ src/main/java/com/dominio/api/model/Pessoa.java
@@ -9,8 +9,10 @@ import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import lombok.Data;
 
 @Entity
+@Data
 public class Pessoa {
 
 	@Id
@@ -20,54 +22,5 @@ public class Pessoa {
 
 	@JsonProperty(access = Access.READ_ONLY)
 	private OffsetDateTime cadastro;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public OffsetDateTime getCadastro() {
-		return cadastro;
-	}
-
-	public void setCadastro(OffsetDateTime cadastro) {
-		this.cadastro = cadastro;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pessoa other = (Pessoa) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 }
