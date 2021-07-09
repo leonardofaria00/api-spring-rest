7692402ba89cbe43e4c770739e353095eb10b15d
Leonardo Faria dos Santos <leonardofaria00@gmail.com>
2021-07-09 17:39:33 -0300

    Adicionado construtores com patametros String e Exception.

--- src/main/java/com/dominio/api/exception/NegocioException.java
+++ src/main/java/com/dominio/api/exception/NegocioException.java
@@ -4,8 +4,15 @@ public class NegocioException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public NegocioException(String mensagem) {
-		super(mensagem);
+	public NegocioException() {
+		super();
 	}
 
+	public NegocioException(String message) {
+		super(message);
+	}
+
+	public NegocioException(String message, Exception erro) {
+		super(message, erro);
+	}
 }
