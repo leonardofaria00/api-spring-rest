15950c85acad7f999c46cb9a7af13bbfcdf711d1
Leonardo Faria dos Santos <leonardofaria00@gmail.com>
2021-07-20 18:11:29 -0300

    Refatorando primary key

--- src/main/java/com/dominio/api/controller/users/PessoaController.java
+++ src/main/java/com/dominio/api/controller/users/PessoaController.java
@@ -1,57 +1,51 @@
 package com.dominio.api.controller.users;
 
-import java.util.List;
-
+import com.dominio.api.dto.user.PessoaDTO;
+import com.dominio.api.exception.NegocioException;
+import com.dominio.api.model.user.Pessoa;
+import com.dominio.api.service.user.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.dominio.api.dto.user.PessoaDTO;
-import com.dominio.api.model.user.Pessoa;
-import com.dominio.api.service.user.PessoaService;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users/v1")
 public class PessoaController {
 
-	@Autowired
-	private PessoaService service;
+    @Autowired
+    private PessoaService service;
 
-	@GetMapping
-	public ResponseEntity<List<PessoaDTO>> listarPessoas() {
-		return service.listarTodos();
-	}
+    @GetMapping
+    public ResponseEntity<List<PessoaDTO>> listarPessoas() {
+        return service.listarTodos();
+    }
 
-	@PostMapping
-	public ResponseEntity<PessoaDTO> cadastrarPessoa(@Validated @RequestBody final Pessoa pessoa) {
-		return service.cadastrar(pessoa);
-	}
+    @PostMapping
+    public ResponseEntity<PessoaDTO> cadastrarPessoa(@Validated @RequestBody final Pessoa pessoa) {
+        return service.cadastrar(pessoa);
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<PessoaDTO> buscarPessoa(@PathVariable final String id) {
-		return service.buscarPorId(id);
-	}
+    @GetMapping("/{uuid}")
+    public ResponseEntity<PessoaDTO> buscarPessoa(@PathVariable final String uuid) {
+        return service.buscarPorId(uuid);
+    }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<PessoaDTO> atualizarPessoa(@PathVariable final String id, @RequestBody final Pessoa pessoa) {
-		return service.atualizarPorId(id, pessoa);
-	}
+    @PutMapping("/{uuid}")
+    public ResponseEntity<PessoaDTO> atualizarPessoa(@PathVariable final String uuid, @RequestBody final Pessoa pessoa) {
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> removerPessoa(@PathVariable final String id) {
-		return service.removerPorId(id);
-	}
+        return service.atualizarPorId(uuid, pessoa);
+    }
 
-	@GetMapping("/mock/{id}")
-	public Pessoa buscarPorIdMock(@PathVariable final Long id) {
-		return service.buscarPorIdMock(id);
-	}
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<Void> removerPessoa(@PathVariable final String uuid) {
+        return service.removerPorId(uuid);
+    }
+
+    @GetMapping("/mock/{uuid}")
+    public Pessoa buscarPorIdMock(@PathVariable final String uuid) {
+        return service.buscarPorIdMock(uuid);
+    }
 }
